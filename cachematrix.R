e21d12ed837724494dc3bf02787b90afe236fb85
Anna <annalisaM@users.noreply.github.com>
2015-07-20 15:04:40 +0300

    Anna's final attempt
    Coursera R-programming course, 2nd assingment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  makeCahceMatrix creates a list that consists of functions.
+##  There are two 'setters' and two 'getters' as follows:
+##  'set' stores the value of the matrix and 'setinversion' its inversion.
+##  'get' returns the value of the matrix and 'getinversion' its inversion.
+##  For an explanation about the inversion of matrices, see e.g.
+##  http://www.purplemath.com/modules/mtrxinvr.htm
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversion <- NULL
+  set <- function(y) {
+    x <<- y
+    inversion <<- NULL
+  }
+  get <- function () x 
+    setinversion <- function(inverse) inversion <<- inverse
+    getinversion <- function() inversion
+    list(set = set, get = get,
+         setinversion = setinversion,
+         getinversion = getinversion)
 }
 
 
-## Write a short comment describing this function
+##  CacheSolve does the inversion, thus its output is the inversion
+##  of the vector/matrix that was sent to it in the function call
+##  NB! In this Coursera assignement, the assumption is that the 
+##  matrix is invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inversion <- x$getinversion()
+        if(!is.null(inversion)) {
+          message("getting cached data")
+          return(inversion)
+        }
+        data <- x$get()
+        #  solve(a, b, ...): 
+        #  a and b are the vectors or matrices.
+        #  if b is missing, solve will return the inverse of a.
+        inversion <- solve(data, ...)
+        x$setinversion(inversion)
+        inversion
 }
+
